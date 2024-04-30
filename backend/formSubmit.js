52fc7fa202717fb560105e8683647ff9a9a681c9
Greg <greg>
2024-04-30 01:08:42 -0700

    improve backend code

--- backend/formSubmit.js
+++ backend/formSubmit.js
@@ -1,63 +1,82 @@
 const axios = require('axios');
-const nodemailer = require("nodemailer");
+const nodemailer = require('nodemailer');
 const startCase = require('lodash.startcase');
-const emails = process.env.EMAILS
-const email = "memoryhole-legal-database@proton.me"
-const minScore = 0.5;
+const fs = require('fs/promises');
+const lockfile = require('proper-lockfile');
+
+const { EMAILS, ENABLE_EMAIL, ENABLE_FILE } = process.env;
+const FROM_EMAIL = 'memoryhole-legal-database@proton.me';
+const MIN_SCORE = 0.5;
+const DATA_PATH = process.env.DATA_PATH || '../data/';
 
 const transporter = nodemailer.createTransport({
-    host: '127.0.0.1',
-    port: 1025,
-    secure: false, // true for 465, false for other ports
-    auth: {
-        user: process.env.PROTONMAIL_LOGIN,
-        pass: process.env.PROTONMAIL_PW,
-    },
-    tls: {
-        rejectUnauthorized: false,
-    },
-})
+  host: '127.0.0.1',
+  port: 1025,
+  secure: false, // True for 465, false for other ports
+  auth: {
+    user: process.env.PROTONMAIL_LOGIN,
+    pass: process.env.PROTONMAIL_PW,
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
 
-const formatEmail = data => {
+const formatEmail = (data) => {
+  // let htmlTable = '<table style="width:100%;border-collapse:collapse;border: 1px solid #ccc;">';
 
-    let htmlTable = '<table style="width:100%;border-collapse:collapse;border: 1px solid #ccc;">';
+  const rows = Object.entries(data).map(([key, value]) => `\n<tr><td style="padding: 8px; border-bottom: 1px solid #ddd;"><b>${startCase(key)}</b></td><td style="padding: 8px; border-bottom: 1px solid #ddd; width:100%;">${value}</td></tr>`).join('');
 
-    for (const [key, value] of Object.entries(data)) {
-        htmlTable += `\n<tr><td style="padding: 8px; border-bottom: 1px solid #ddd;"><b>${startCase(key)}</b></td><td style="padding: 8px; border-bottom: 1px solid #ddd; width:100%;">${value}</td></tr>`;
-    }
+  return `<table style="width:100%;border-collapse:collapse;border: 1px solid #ccc;">${rows}</table>`;
+};
 
-    htmlTable += '</table>';
-    return htmlTable
-}
-
-// async..await is not allowed in global scope, must use a wrapper
+// Async..await is not allowed in global scope, must use a wrapper
 async function send_email(data) {
-    // send mail with defined transport object
-    const info = await transporter.sendMail({
-        from: `MemoryHole Legal DB" <${email}>`, // sender address
-        to: emails, // list of receivers
-        subject: "New Arrestee Intake", // Subject line
-        html: formatEmail(data), // html body
-    });
+  // Send mail with defined transport object
+  const info = await transporter.sendMail({
+    from: `MemoryHole Legal DB" <${FROM_EMAIL}>`, // Sender address
+    to: EMAILS, // List of receivers
+    subject: 'New Arrestee Intake', // Subject line
+    html: formatEmail(data), // Html body
+  });
 
-    console.log("Message sent: %s", info.messageId);
+  console.log('Message sent: %s', info.messageId);
 }
 
 const captcha = async (token) => {
-    const secret_key = process.env.CAPTCHA_SECRET_KEY;
-    const url = `https://www.google.com/recaptcha/api/siteverify`;
-    const { data: { success, score } } = await axios.post(url, null, { params: { secret: secret_key, response: token } })
-    if (!success || score < minScore) {
-        return Promise.reject("Captcha validation failed")
-    }
-}
+  const secretKey = process.env.CAPTCHA_SECRET_KEY;
+  const url = 'https://www.google.com/recaptcha/api/siteverify';
+  const {
+    data: { success, score },
+  } = await axios.post(url, null, {
+    params: { secret: secretKey, response: token },
+  });
+  if (!success || score < MIN_SCORE) {
+    return Promise.reject(new Error('Captcha validation failed'));
+  }
+  return score;
+};
+
+const write_file = async (data) => {
+  const filename = `${DATA_PATH}${new Date().toJSON()}.json`;
+  const json_data = JSON.stringify(data);
+  const fh = await fs.open(filename, 'a');
+  await fh.close();
+  await lockfile.lock(filename);
+  await fs.writeFile(filename, json_data);
+  await fs.chmod(filename, 0o600);
+  await lockfile.unlock(filename);
+};
 
 const formSubmit = async (req, res) => {
-    const { data, token } = req.body
-    console.log('processing')
-    await captcha(token)
-    await send_email(data)
-    return res.send('ok')
-}
+  const { data, token } = req.body;
+  console.log('processing');
+  await captcha(token);
+  await Promise.all([
+    ENABLE_EMAIL && send_email(data),
+    ENABLE_FILE && write_file(data),
+  ]);
+  return res.send('ok');
+};
 
-module.exports = formSubmit
+module.exports = formSubmit;
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,9 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
-const router = require('express-promise-router')()
-const formSubmit = require('./formSubmit')
+const router = require('express-promise-router')();
+const formSubmit = require('./formSubmit');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -11,8 +12,8 @@ app.use(bodyParser.json());
 router.post('/submit', formSubmit);
 app.use(router);
 app.use((err, req, res, next) => {
-  console.error(err)
-  console.error(err.stack)
-  res.status(500).json({ error: err })
-})
+  console.error(err);
+  console.error(err.stack);
+  res.status(500).json({ error: err.message || err });
+});
 app.listen(port, () => console.log(`Listening on port ${port}!`));
